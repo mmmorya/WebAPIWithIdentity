5ea5e8a4e4b03a429f7c0c1a3560c50385b99b90
mmmorya <mmmorya@oforce.com>
2024-05-11 15:06:49 +0530

    Add project files.

---
+++ WebAPIWithIdentity/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAPIWithIdentity.Models;
+
+namespace WebAPIWithIdentity.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
